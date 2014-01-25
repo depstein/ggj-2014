b27bed2135440083b2c29f249f3b2fe807df1360
Ben Witman <benwitman@benwitman.com>
2014-01-24 20:53:22 -0800

    moving

---
+++ code/Assets/Scripts/Camera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	Vector2 direction;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject player = GameObject.Find ("Player");
+
+		direction = transform.position + (player.transform.position - transform.position) / 2;
+	}
+
+	void FixedUpdate()
+	{
+		transform.position = new Vector3(direction.x, direction.y, transform.position.z);
+	}
+}
