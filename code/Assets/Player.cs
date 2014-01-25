ef8ea7ed0a6152d10eaf3da0c6d6e9adb8e59604
Ben Witman <benwitman@benwitman.com>
2014-01-24 20:16:43 -0800

    Player!

---
+++ code/Assets/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private Vector2 speed = new Vector2(50, 50);
+	private Vector2 direction;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		var horizontal = Input.GetAxis ("Horizontal") * speed.x;
+		var vertical = Input.GetAxis ("Vertical") * speed.y;
+
+		direction = new Vector2 (horizontal, vertical);
+	}
+
+	void FixedUpdate()
+	{
+		var old_direction = rigidbody2D.velocity;
+
+		var diff = direction - old_direction;
+
+		rigidbody2D.AddForce (diff);
+		rigidbody2D.velocity = direction - diff / 2;
+	}
+
+	
+}
