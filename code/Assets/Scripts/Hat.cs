1060bd1fd053c478a06676fb56cee6319d69a3be
Daniel Epstein <depstein@cs.washington.edu>
2014-01-25 21:57:21 -0800

    Changing world mode!

--- code/Assets/Scripts/Hat.cs
+++ code/Assets/Scripts/Hat.cs
@@ -20,14 +20,10 @@ public class Hat : MonoBehaviour {
 			}
 			switch(this.gameObject.name) { //TODO: this will fail when we start cloning things (aka making them dynamically)
 				case "Archer Hat":
-					other.gameObject.AddComponent<Archer>();
-					GameArea.gameArea.TurnGoodThingsTo(Resources.Load<GameObject>("archer-good-pickup"));
-					GameArea.gameArea.TurnBadThingsTo(Resources.Load<GameObject>("Rabbit"));
+					WorldMode.ChangeModeTo(other.gameObject, PlayerMode.archer);
 				    break;
 				case "shepherd-staff":
-					other.gameObject.AddComponent<Shepherd>();
-					GameArea.gameArea.TurnGoodThingsTo(Resources.Load<GameObject>("sheep"));
-					//GameArea.gameArea.TurnBadThingsTo(Resources.Load<GameObject>("Rabbit"));
+				WorldMode.ChangeModeTo(other.gameObject, PlayerMode.shepherd);
 					break;
 			}
 			Destroy (this.gameObject);
