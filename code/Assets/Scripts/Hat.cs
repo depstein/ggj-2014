5daf5af6a372d111725f22abfeca5eeebfb4c204
Daniel Epstein <depstein@cs.washington.edu>
2014-01-25 16:49:36 -0800

    Added some good/bad objects.

---
+++ code/Assets/Scripts/Hat.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hat : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.GetComponent<Player> () != null) {
+			Destroy (this.gameObject);
+		}
+	}
+}
