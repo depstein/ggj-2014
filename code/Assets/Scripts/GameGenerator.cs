809e5188bb81475b14a9db84550e45a3bf318069
Ben Witman <benwitman@benwitman.com>
2014-01-25 21:46:12 -0800

    moving GameGenerator to become Main

--- code/Assets/Scripts/GameGenerator.cs
+++ code/Assets/Scripts/GameGenerator.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -11,24 +11,24 @@ public class GameGenerator : MonoBehaviour
 	
 	private GameObject player_object;
 	private Player player;
-
+	
 	void Awake()
 	{
 		Wall.WallTemplate = WallTemplate;
 		Wall.DebugMaterialTemplate = DebugMaterialTemplate;
 		WorldManager.worldManager = new WorldManager ();
 	}
-
+	
 	void Start () 
 	{
 		WorldManager.worldManager.Load ();
 		var area = WorldManager.worldManager.PickRandomArea ();
-
+		
 		player_object = Instantiate (PlayerTemplate, area.position, Quaternion.identity) as GameObject;
 		player = player_object.GetComponent<Player> ();
-
+		
 		var camera = GameObject.Find ("Main Camera") as GameObject;
-
+		
 		var player_position = player_object.transform.position;
 		camera.transform.position = new Vector3(player_position.x, player_position.y, camera.transform.position.z);
 	}
