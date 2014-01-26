8bb8c948f5958e2767bfc1f5103c238b4593d9b6
Daniel Epstein <depstein@cs.washington.edu>
2014-01-25 16:18:24 -0800

    Quiver for arrows

--- code/Assets/Scripts/Professions/Archer.cs
+++ code/Assets/Scripts/Professions/Archer.cs
@@ -9,6 +9,7 @@ public class Archer : MonoBehaviour {
 	public GameObject whatIFire;
 	private Vector3 _projectileDirection;
 	private AnimatedCharacter _animatedCharacter; 
+	private int ammoCount = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -27,7 +28,8 @@ public class Archer : MonoBehaviour {
 
 	void CheckFire()
 	{
-		if (Input.GetMouseButtonDown (0)) {
+		if (Input.GetMouseButtonDown (0) && ammoCount > 0) {
+			ammoCount--;
 			_projectileDirection = _player.mouseDirection;
 			_animator.SetTrigger("Attack");
 		}
@@ -39,5 +41,13 @@ public class Archer : MonoBehaviour {
 		var projectile = (GameObject)Instantiate (whatIFire, _mouth.position + 0.5f * _projectileDirection, transform.rotation);
 		projectile.transform.rotation = Quaternion.FromToRotation(Vector3.up, _projectileDirection);
 		projectile.rigidbody2D.velocity = 20 * _projectileDirection;
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.name == "archer-good-pickup") {
+			Destroy (other.gameObject);
+			ammoCount++; //TODO: should we put more than one arrow in the quiver?
+		}
 	}
 }
