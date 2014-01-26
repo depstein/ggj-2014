a35c2009402b0dab165d2d3dd2ad3daefa040843
Ben Witman <benwitman@benwitman.com>
2014-01-25 21:41:46 -0800

    camera is following again (SOMEBODY's WATCHING YOU!)

--- code/Assets/Scripts/Player.cs
+++ code/Assets/Scripts/Player.cs
@@ -25,6 +25,8 @@ public class Player : MonoBehaviour {
 
 		_pupil = transform.Find ("body/front/pupil").gameObject;
 
+		Camera.player = gameObject;
+
 		//gameObject.AddComponent<Shepherd>();
 		//GetComponent<Archer>().whatIFire = (GameObject)Resources.Load("archer-projectile", typeof(GameObject));
 	}
